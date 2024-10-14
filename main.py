59ffbb6308c22c56646c39ac045660be85ccd30b
tvey <liteitt@gmail.com>
2024-10-14 22:46:56 +0300

    Try echo and add some dependencies

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
+import os
+
+import dotenv
+import validators
 from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
+
+dotenv.load_dotenv()
 
 
-async def henlo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text('Henlo!')
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.message.text
+
+    if validators.url(message):
+        await update.message.reply_text("It's a link!")
+    else:
+        await update.message.reply_text(message)
 
 
 def main() -> None:
-    application = Application.builder().token(BOT_TOKEN).build()
-    application.add_handler(CommandHandler('henlo', henlo))
+    application = Application.builder().token(os.environ.get('BOT_TOKEN')).build()
+    # application.add_handler(CommandHandler('start', start))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
     application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
